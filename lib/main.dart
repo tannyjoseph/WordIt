208e2b229fd18526c0c3a1d9ec7c3625445ba3d6
Tania Joseph <taniajoseph37@gmail.com>
2020-06-21 19:19:39 +0530

    Bottom Navigation Bar changed to TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -22,67 +22,86 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    //load(list, meanings);
+    load(list, meanings);
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
+        home: DefaultTabController(
+        length: 5,
+          child: Scaffold(
+          appBar: AppBar(
           backgroundColor: Color(0xFF84DCC6),
-          title: Text("WordIt"),
-        ),
-        bottomSheet: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: Color(0xFFFF686B),
-          unselectedItemColor: Colors.grey[600],
-          items: <BottomNavigationBarItem>[
-            getTab(Icons.collections_bookmark),
-            getTab(Custom.cards),
-            getTab(Custom.sentences),
-            getTab(Custom.synonyms),
-            getTab(Custom.fun)
-          ],
-          currentIndex: _selectedIndex,
-          onTap: onTabTapped,
-        ),
-        body: Center(
-          child: PageView.builder(
-            itemBuilder: (context, position) {
-              return Container(
-                child: Center(
-                  child: Column(
-                    children: [
-//                        Image(
-//                            image: NetworkImage(
-//                                "https://cdn.vox-cdn.com/thumbor/fNwRmSTE0L-p6SjuaQmsmhfmEb0=/0x0:655x365/1400x1400/filters:focal(249x23:353x127):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/55356659/spider_man_homecoming.0.jpg")),
-                      Text(
-                        list[position],
-                        style: TextStyle(color: Colors.black, fontSize: 50.0),
-                      ),
-                      Text(
-                        meanings[position],
-                        style: TextStyle(color: Colors.black, fontSize: 25.0),
-                      )
-                    ],
-                  ),
-                ),
-              );
-            },
-            itemCount: list.length,
-          ),
-        ),
+    title: Text("WordIt"),
+    ),
+    bottomNavigationBar: new TabBar(
+    tabs: [
+    getTab(Icons.collections_bookmark),
+    getTab(Custom.cards),
+    getTab(Custom.sentences),
+    getTab(Custom.synonyms),
+    getTab(Custom.fun)
+    ],
+//    onTap: onTabTapped,
+    labelColor: Colors.yellow,
+    unselectedLabelColor: Colors.blue,
+    indicatorSize: TabBarIndicatorSize.label,
+    indicatorPadding: EdgeInsets.all(5.0),
+    indicatorColor: Colors.red,
+    ),
+    body: TabBarView(
+      
+    children: [new Container(
+    color: Colors.yellow,
+    ),
+    new Container(
+    color: Colors.orange,
+      child: Text(list[0], style: TextStyle(color: Colors.black, fontSize: 50.0),
       ),
-    );
+    ),
+    new Container(
+    color: Colors.lightGreen,
+    ),
+    new Container(
+    color: Colors.red,
+    ),
+      new Container(
+        color: Colors.purple,
+      )
+    ],
+
+    )
+//          child: PageView.builder(
+//            itemBuilder: (context, position) {
+//              return Container(
+//                child: Center(
+//                  child: Column(
+//                    children: [
+////                        Image(
+////                            image: NetworkImage(
+////                                "https://cdn.vox-cdn.com/thumbor/fNwRmSTE0L-p6SjuaQmsmhfmEb0=/0x0:655x365/1400x1400/filters:focal(249x23:353x127):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/55356659/spider_man_homecoming.0.jpg")),
+//                      Text(
+//                        list[position],
+//                        style: TextStyle(color: Colors.black, fontSize: 50.0),
+//                      ),
+//                      Text(
+//                        meanings[position],
+//                        style: TextStyle(color: Colors.black, fontSize: 25.0),
+//                      )
+//                    ],
+//                  ),
+//                ),
+    ),
+        ));
   }
 
-  BottomNavigationBarItem getTab(IconData icon) {
-    return BottomNavigationBarItem(
-        icon: Icon(icon), activeIcon: Icon(icon), title: Text(('')));
-  }
 
-  void onTabTapped(int value) {
-    setState(() {
-      _selectedIndex = value;
-    });
-  }
 }
+
+Tab getTab(IconData icon) {
+  return Tab(
+      icon: Icon(icon));
+}
+
+//void onTabTapped(int value) {
+//  setState(() {
+//    _selectedIndex = value;
+//  });
+//}}
